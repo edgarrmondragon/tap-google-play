1d093334431ed4e79ee59d105acd36ea6c30a215
arthurto <arthur.ps22009@gmail.com>
2022-08-15 14:39:57 -0300

    tap redesigned

--- tap_google_play/streams.py
+++ tap_google_play/streams.py
@@ -1,27 +1,60 @@
-"""Stream type classes for tap-google-play."""
-
-from pathlib import Path
+from tracemalloc import start
 from typing import Any, Dict, Optional, Union, List, Iterable
 
-from singer_sdk import typing as th  # JSON Schema typing helpers
+from singer_sdk import typing as th 
 
 from tap_google_play.client import GooglePlayStream
 
+import datetime
+
+from google_play_scraper import reviews, Sort
 
 class ReviewsStream(GooglePlayStream):
+    """Define custom stream."""
     name = "reviews"
-    path = "/reviews"
-    primary_keys = ["reviewId"]
-    replication_key = None
-    records_jsonpath = "$.reviews[*]"
-
+    primary_keys = ["id"]
+    replication_key = "at"
     schema = th.PropertiesList(
-        th.Property("reviewId", th.StringType),
-        th.Property("authorName", th.StringType),
-        th.Property("comments", th.ArrayType(
-            th.CustomType({
-                "type": ["object", "string", "null"]
-            })
-        ))
+        th.Property("userName", th.StringType),
+        th.Property("userImage",th.StringType),
+        th.Property("content",th.StringType),
+        th.Property("score",th.IntegerType),
+        th.Property("thumbsUpCount",th.IntegerType),
+        th.Property("reviewCreatedVersion",th.StringType),
+        th.Property("at",th.DateTimeType),
+        th.Property("replyContent",th.StringType),
+        th.Property("repliedAt",th.DateTimeType),
+        th.Property("reviewId",th.StringType),
     ).to_dict()
 
+    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
+
+        app_id = self.config.get("app_id")
+
+        continuation_token = None
+        results = []
+        result = True
+
+        start_date = self.get_starting_replication_key_value(context)
+        if start_date: start_date =  datetime.datetime.strptime(
+                                    start_date,'%Y-%m-%dT%H:%M:%S')
+
+        self.logger.info(f"Getting reviews for {app_id}.")
+        while result: 
+
+            result,continuation_token = reviews(
+                app_id, lang = 'en', country = 'us',
+                sort = Sort.NEWEST,
+                count = 1000,continuation_token=continuation_token)
+                
+            if start_date:
+                for record in result:
+                    if record.get('at') > start_date: 
+                        results.append(record)
+                    else: result = None
+            else: 
+                results += result
+            
+            self.logger.info(f"{results.__len__()} imported records so far.")
+
+        return results 
--- tap_google_play/tap.py
+++ tap_google_play/tap.py
@@ -1,45 +1,31 @@
-"""GooglePlay tap class."""
-
 from typing import List
 
 from singer_sdk import Tap, Stream
-from singer_sdk import typing as th  # JSON schema typing helpers
+from singer_sdk import typing as th
+from tap_google_play.streams import (ReviewsStream)
 
-from tap_google_play.streams import (
-    GooglePlayStream,
-    ReviewsStream 
-)
-
-STREAM_TYPES = [
-    ReviewsStream
-]
-
+STREAM_TYPES = [ReviewsStream]
 
 class TapGooglePlay(Tap):
     """GooglePlay tap class."""
-    name = "tap-google-play"
+    name = "tap-googleplay"
 
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "access_token",
-            th.StringType
-        ),
-        th.Property(
-            "refresh_token",
-            th.StringType
-        ),
-        th.Property(
-            "package_name",
+            "app_id",
             th.StringType,
             required=True
         ),
         th.Property(
             "start_date",
             th.DateTimeType,
-            description="The earliest record date to sync"
+            required=False
         )
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
+
+if __name__ == '__main__':
+    TapGooglePlay.cli()
