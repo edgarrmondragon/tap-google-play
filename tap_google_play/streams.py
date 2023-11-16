bb117d057743bb70ba343d298429ac66bfaff7e8
pnadolny13 <patnadolny@gmail.com>
2023-11-16 17:27:44 -0500

    wip multiple apps

--- tap_google_play/streams.py
+++ tap_google_play/streams.py
@@ -1,12 +1,15 @@
 """Stream type classes for tap-google-play."""
 
 from __future__ import annotations
+from os import PathLike
 
 from typing import Iterable
 
-from google_play_scraper import Sort, reviews
+from google_play_scraper import Sort, reviews, app
 from pendulum import parse
 from singer_sdk import typing as th
+import singer_sdk._singerlib as singer
+from singer_sdk.tap_base import Tap
 
 from tap_google_play.client import GooglePlayStream
 
@@ -14,7 +17,10 @@ from tap_google_play.client import GooglePlayStream
 class ReviewsStream(GooglePlayStream):
     """Define custom stream."""
 
-    name = "reviews"
+    def __init__(self, *args, **kwargs) -> None:
+        self.app_id = kwargs.pop("app_id")
+        super().__init__(*args, **kwargs)
+        
     primary_keys = ["reviewId"]  # noqa: RUF012
     replication_key = "at"
     schema = th.PropertiesList(
@@ -29,23 +35,29 @@ class ReviewsStream(GooglePlayStream):
         th.Property("repliedAt", th.DateTimeType),
         th.Property("reviewId", th.StringType),
         th.Property("appVersion", th.StringType),
+        th.Property("appId", th.StringType),
+        th.Property("developerId", th.StringType),
     ).to_dict()
 
     def get_records(self, context: dict | None) -> Iterable[dict]:
         """Return a generator of row-type dictionary objects."""
-        app_id = self.config.get("app_id")
-
         continuation_token = None
-
+        app_details = app(
+            self.app_id,
+            lang='en', # defaults to 'en'
+            country='us' # defaults to 'us'
+        )
+        context = {"developerId": app_details["developerId"], "appId": self.app_id}
         start_date = self.get_starting_replication_key_value(context)
         if start_date:
             start_date = parse(start_date)
 
-        self.logger.info("Getting reviews for %s", app_id)
+        self.logger.info("Getting reviews for %s", self.app_id)
 
+        it = 0
         while True:
             result, continuation_token = reviews(
-                app_id,
+                self.app_id,
                 lang="en",
                 country="us",
                 sort=Sort.NEWEST,
@@ -55,10 +67,32 @@ class ReviewsStream(GooglePlayStream):
 
             if not result:
                 break
-
+            if it > 0:
+                break
             if start_date:
                 for record in result:
                     if record.get("at") > start_date.replace(tzinfo=None):
                         yield record
             else:
                 yield from result
+            it += 1
+
+    def post_process(
+        self,
+        row: dict,
+        context: dict | None = None,  # noqa: ARG002
+    ) -> dict | None:
+        # row
+        row["developerId"] = context["developerId"]
+        row["appId"] = context["appId"]
+        return super().post_process(row, context)
+    
+    # def post_process(
+    #     self,
+    #     row: dict,
+    #     context: dict | None = None,  # noqa: ARG002
+    # ) -> dict | None:
+    #     # row
+    #     row["developerId"] = context["developerId"]
+    #     row["appId"] = context["appId"]
+    #     return super().post_process(row, context)
--- tap_google_play/tap.py
+++ tap_google_play/tap.py
@@ -17,7 +17,13 @@ class TapGooglePlay(Tap):
         th.Property(
             "app_id",
             th.StringType,
-            required=True,
+            # required=True,
+            description="The app ID to extract reviews from",
+        ),
+        th.Property(
+            "app_id_list",
+            th.ArrayType(th.StringType),
+            # required=True,
             description="The app ID to extract reviews from",
         ),
         th.Property(
@@ -30,9 +36,13 @@ class TapGooglePlay(Tap):
 
     def discover_streams(self) -> list[Stream]:
         """Return a list of discovered streams."""
-        return [
-            streams.ReviewsStream(tap=self),
-        ]
+        stream_list = []
+        if self.config.get("app_id_list"):
+            for app_id in self.config.get("app_id_list"):
+                stream_list.append(streams.ReviewsStream(tap=self, name=app_id, app_id=app_id))
+        else:
+            stream_list.append(streams.ReviewsStream(tap=self, name="reviews", app_id=self.config.get("app_id")))
+        return stream_list
 
 
 if __name__ == "__main__":
