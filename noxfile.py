490e1b19ee0b21485deb3f6b173d0cad9a29892f
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-08-25 16:46:29 -0600

    chore: Use `uv` instead of `poetry`

--- noxfile.py
+++ noxfile.py
@@ -2,20 +2,7 @@
 
 from __future__ import annotations
 
-import os
-import sys
-from textwrap import dedent
-
 import nox
-
-try:
-    from nox_poetry import Session, session
-except ImportError:
-    message = f"""\
-    Nox failed to import the 'nox-poetry' package.
-    Please install it using the following command:
-    {sys.executable} -m pip install nox-poetry"""
-    raise SystemExit(dedent(message)) from None
 
 python_versions = [
     "3.13",
@@ -28,20 +15,14 @@ python_versions = [
 nox.options.sessions = ("tests",)
 
 
-@session(python=python_versions)
-def tests(session: Session) -> None:
+@nox.session(python=python_versions)
+def tests(session: nox.Session) -> None:
     """Execute pytest tests."""
-    deps = ["pytest", "pytest-durations"]
-    if "GITHUB_ACTIONS" in os.environ:
-        deps.append("pytest-github-actions-annotate-failures")
-
-    session.install(".")
-    session.install(*deps)
-    session.run("pytest", *session.posargs)
+    session.run("uv", "run", "--python", f"python{session.python}", "pytest")
 
 
-@session
-def mypy(session: Session) -> None:
+@nox.session
+def mypy(session: nox.Session) -> None:
     """Check types."""
     deps = ["mypy"]
     args = session.posargs or ("tap_google_play",)
